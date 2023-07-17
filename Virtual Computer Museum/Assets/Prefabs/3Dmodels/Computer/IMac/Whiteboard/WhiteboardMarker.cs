5344091acf35fb43fc5c159f12b8ff9c3fdcfea9
DatenLauch <103434272+DatenLauch@users.noreply.github.com>
2023-07-17 23:58:14 +0200

    LayoutFix3 + QuizRotator + Tutorial

--- Virtual Computer Museum/Assets/Prefabs/3Dmodels/Computer/IMac/Whiteboard/WhiteboardMarker.cs
+++ Virtual Computer Museum/Assets/Prefabs/3Dmodels/Computer/IMac/Whiteboard/WhiteboardMarker.cs
@@ -18,7 +18,7 @@ public class WhiteboardMarker : MonoBehaviour
     private Vector2 _touchPos, _lastTouchPos;
     private bool _touchedLastFrame;
     private Quaternion _lastTouchRot;
-    
+
     void Start()
     {
         _renderer = _tip.GetComponent<Renderer>();
@@ -47,7 +47,7 @@ public class WhiteboardMarker : MonoBehaviour
                 var x = (int)(_touchPos.x * _whiteboard.textureSize.x - (_penSize / 2));
                 var y = (int)(_touchPos.y * _whiteboard.textureSize.y - (_penSize / 2));
 
-                if (y < 0 || y > _whiteboard.textureSize.y || x < 0 || x > _whiteboard.textureSize.x) return;
+                if (y < 0 || y + _penSize > _whiteboard.textureSize.y || x < 0 || x + _penSize > _whiteboard.textureSize.x) return;
 
                 if (_touchedLastFrame)
                 {
@@ -57,11 +57,14 @@ public class WhiteboardMarker : MonoBehaviour
                     {
                         var lerpX = (int)Mathf.Lerp(_lastTouchPos.x, x, f);
                         var lerpY = (int)Mathf.Lerp(_lastTouchPos.y, y, f);
+
+                        if (lerpY < 0 || lerpY + _penSize > _whiteboard.textureSize.y || lerpX < 0 || lerpX + _penSize > _whiteboard.textureSize.x) continue;
+
                         _whiteboard.texture.SetPixels(lerpX, lerpY, _penSize, _penSize, _colors);
                     }
 
                     transform.rotation = _lastTouchRot;
-                    
+
                     _whiteboard.texture.Apply();
                 }
 
