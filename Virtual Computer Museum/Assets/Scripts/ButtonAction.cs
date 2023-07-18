3041af6dc7ffd8d46f871510c215b29808f9b0c1
Thien Duong <duongthien1809@gmail.com>
2023-07-18 10:18:16 +0200

    edit gamescene

--- Virtual Computer Museum/Assets/Scripts/ButtonAction.cs
+++ Virtual Computer Museum/Assets/Scripts/ButtonAction.cs
@@ -1,15 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class ButtonAction : MonoBehaviour
 {
     public string sceneName;
+    public GameObject objectWithButtonActionScript;
+
     public void buttonAction()
     {
-        Debug.Log("Done!");
-        SceneManager.LoadScene(sceneName);
+        
+        if (!(sceneName == "Museum"))
+        {
+            // Speichere die aktuelle Position und die Szene in der Datenklasse
+            //PositionDataManager.SetPreviousPosition(objectWithButtonActionScript.transform.position);
+            //Debug.Log("gespeichert! "+objectWithButtonActionScript.transform.position);
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.Log("Done!");
+            //objectWithButtonActionScript.transform.position = PositionDataManager.GetPreviousPosition();
+            objectWithButtonActionScript.transform.position = new Vector3(-1.18f, 0f, 17.4f);
+            SceneManager.LoadScene(sceneName);
+           
+        }
+        
     }
 }
